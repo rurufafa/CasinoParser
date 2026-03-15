2ae00f6368cc3a881a9686934ee4b5dbe98bbfac
rurufafa <214452817+rurufafa@users.noreply.github.com>
2026-03-15 23:25:13 +0900

    debug

--- parser.js
+++ parser.js
@@ -39,6 +39,7 @@ export default class LogParser {
                     exitsId.push(id);
                 idMap.status.push(id);
             } else if (this.isInCasino) {
+                console.log(log);
                 idMap[log.type].push(id);
             }
         }
