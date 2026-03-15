631827a5f871c1c65b67c301400dca875d9e5888
rurufafa <214452817+rurufafa@users.noreply.github.com>
2026-03-15 23:45:17 +0900

    a

--- matcher.js
+++ matcher.js
@@ -104,7 +104,7 @@ export default class LogMatcher {
 
         if (barBuy) {
             const name = barBuy[1].trim();
-            console.log(clean, name);
+            console.log(line, clean, name);
             return { type: "bar", direction: "pay", name };
         }
 
